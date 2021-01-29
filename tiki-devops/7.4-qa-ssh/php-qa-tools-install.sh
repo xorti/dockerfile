600ab12fe9117823ab58ab0f0c5f76edcb5bc03d
Jorge Sá Pereira <1307414+xorti@users.noreply.github.com>
2021-01-29 15:30:34 +0000

    Add new tiki-devops/7.4-qa-ssh

---
+++ tiki-devops/7.4-qa-ssh/php-qa-tools-install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#
+# Composer
+#
+
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --install-dir=/usr/local/bin --filename=composer
+RESULT=$?
+rm composer-setup.php
+
+#
+# Install Box
+#
+
+wget -O /usr/local/bin/box https://github.com/humbug/box/releases/download/3.8.4/box.phar
+chmod a+x /usr/local/bin/box
+
+exit $RESULT
+
+#
+# PHP Unit
+#
+
+if [ $(php -r "echo version_compare(PHP_VERSION, '5.0.0', '>=') ? 'OK' : 'NOK';") == "OK" ] ; then
+	wget -O /usr/local/bin/phpunit-4 https://phar.phpunit.de/phpunit-4.phar
+	chmod a+x /usr/local/bin/phpunit-4
+fi
+if [ $(php -r "echo version_compare(PHP_VERSION, '5.6.0', '>=') ? 'OK' : 'NOK';") == "OK" ] ; then
+	wget -O /usr/local/bin/phpunit-5 https://phar.phpunit.de/phpunit-5.phar
+	chmod a+x /usr/local/bin/phpunit-5
+fi
+if [ $(php -r "echo version_compare(PHP_VERSION, '7.0.0', '>=') ? 'OK' : 'NOK';") == "OK" ] ; then
+	wget -O /usr/local/bin/phpunit-6 https://phar.phpunit.de/phpunit-6.phar
+	chmod a+x /usr/local/bin/phpunit-6
+fi
+if [ $(php -r "echo version_compare(PHP_VERSION, '7.1.0', '>=') ? 'OK' : 'NOK';") == "OK" ] ; then
+	wget -O /usr/local/bin/phpunit-7 https://phar.phpunit.de/phpunit-7.phar
+	chmod a+x /usr/local/bin/phpunit-7
+fi
+
+
+
+#
+# Install paralle lint
+#
